afd4c2ebbf5b6fedf6ce9997b26b8a39e9be5c30
lihe <lihellowd@outlook.com>
2021-01-08 22:51:38 +0800

    👀

--- scatter_squares_3.py
+++ scatter_squares_3.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 x_value = list(range(1, 1001))
 y_value = [x**2 for x in x_value]  # 平方值
 
-plt.scatter(x_value, y_value, edgecolors='none', s=10)
-
+# plt.scatter(x_value, y_value, c='red', edgecolors='none', s=10)
+plt.scatter(x_value, y_value, c=y_value, cmap=plt.cm.Blues, edgecolors='none', s=10)  # 渐变色
 # 设置图表标题并给坐标加上标签
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
@@ -13,6 +13,7 @@ plt.ylabel("Square of Value", fontsize=14)
 # 设置刻度标记大小
 plt.tick_params(axis='both', which='major', labelsize=14)
 
-plt.axis([0, 1100, 0, 1100000])
+plt.axis([0, 1100, 0, 1100000])  # x和y轴坐标的最小最大值
 
-plt.show()
+# plt.show()
+plt.savefig('squares_plot.png')  # 保存图表
